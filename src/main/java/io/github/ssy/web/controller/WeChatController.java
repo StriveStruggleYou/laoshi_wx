0dcc67f0c7c608dec4cf4c7d3a56a9aa1a32fd42
StriveStruggleYou <StriveStruggleYou@163.com>
2019-03-15 11:27:46 +0800

    新增日志信息

--- src/main/java/io/github/ssy/web/controller/WeChatController.java
+++ src/main/java/io/github/ssy/web/controller/WeChatController.java
@@ -19,56 +19,57 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class WeChatController {
 
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+  Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping("/")
-    @ResponseBody
-    public Object msgDeal(String echostr, String signature, String timestamp, String nonce,
-            String openid, HttpServletRequest request) {
-        if (StringUtils.isEmpty(echostr)) {
-            String postdata = null;
-            try {
-                postdata = inputStreamToString(request.getInputStream());
-                logger.warn("postdata" + postdata);
-                Document doc = DocumentHelper.parseText(postdata);
-                Element root = doc.getRootElement();
-                return buidSendMessage(root.elementText("FromUserName"));
-            } catch (Exception e) {
-                logger.error("postdata error openId" + openid, e);
-            }
-            return "success";
-        } else {
-            //校验token 逻辑
-            return echostr;
-        }
+  @RequestMapping("/")
+  @ResponseBody
+  public Object msgDeal(String echostr, String signature, String timestamp, String nonce,
+      String openid, HttpServletRequest request) {
+    if (StringUtils.isEmpty(echostr)) {
+      String postdata = null;
+      try {
+        postdata = inputStreamToString(request.getInputStream());
+        logger.warn("postdata" + postdata);
+        Document doc = DocumentHelper.parseText(postdata);
+        Element root = doc.getRootElement();
+        logger.warn("root:" + root.asXML());
+        return buidSendMessage(root.elementText("FromUserName"));
+      } catch (Exception e) {
+        logger.error("postdata error openId" + openid, e);
+      }
+      return "success";
+    } else {
+      //校验token 逻辑
+      return echostr;
     }
+  }
 
-    public String inputStreamToString(InputStream is) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int i;
-        while ((i = is.read()) != -1) {
-            baos.write(i);
-        }
-        return baos.toString();
+  public String inputStreamToString(InputStream is) throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    int i;
+    while ((i = is.read()) != -1) {
+      baos.write(i);
     }
+    return baos.toString();
+  }
 
 
-    private String buidSendMessage(String openId) {
-        StringBuffer str = new StringBuffer();
-        String type = "text";
-        String backMsg = "http://laoshi.yidaren.top//learning/qiandao.html";
-        str.append("<xml>");
-        str.append("<ToUserName>" + openId + "</ToUserName>");
-        str.append("<FromUserName>gh_3678a48f4fca</FromUserName>");
-        str.append("<CreateTime>" + System.currentTimeMillis() + "</CreateTime>");
-        str.append("<MsgType>" + type + "</MsgType>");
-        str.append("<Content>" + backMsg + "</Content>");
-        str.append("</xml>");
-        logger.info("appidCallBack method sync weixin user "
-                + "info to dalin account success:" + str.toString());
-        return str.toString();
+  private String buidSendMessage(String openId) {
+    StringBuffer str = new StringBuffer();
+    String type = "text";
+    String backMsg = "http://laoshi.yidaren.top//learning/qiandao.html";
+    str.append("<xml>");
+    str.append("<ToUserName>" + openId + "</ToUserName>");
+    str.append("<FromUserName>gh_3678a48f4fca</FromUserName>");
+    str.append("<CreateTime>" + System.currentTimeMillis() + "</CreateTime>");
+    str.append("<MsgType>" + type + "</MsgType>");
+    str.append("<Content>" + backMsg + "</Content>");
+    str.append("</xml>");
+    logger.info("appidCallBack method sync weixin user "
+        + "info to dalin account success:" + str.toString());
+    return str.toString();
 
-    }
+  }
 
 
 }
