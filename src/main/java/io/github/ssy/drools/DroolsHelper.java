2bd9c73f0e0d376cbf2065a2f7063a62b2a4532c
StriveStruggleYou <StriveStruggleYou@163.com>
2019-03-16 14:22:36 +0800

    提交基础数据信息

--- src/main/java/io/github/ssy/drools/DroolsHelper.java
+++ src/main/java/io/github/ssy/drools/DroolsHelper.java
@@ -6,13 +6,14 @@ import org.kie.api.runtime.KieSession;
 
 public class DroolsHelper {
 
-  KieServices ks = KieServices.Factory.get();
 
-  // From the kie services, a container is created from the classpath
-  KieContainer kc = ks.getKieClasspathContainer();
 
 
   public KieSession getKieSession() {
+    KieServices ks = KieServices.Factory.get();
+
+    // From the kie services, a container is created from the classpath
+    KieContainer kc = ks.getKieClasspathContainer();
     KieSession ksession = kc.newKieSession("BindMobile");
     return ksession;
   }
--- src/main/java/io/github/ssy/web/controller/DangKeController.java
+++ src/main/java/io/github/ssy/web/controller/DangKeController.java
@@ -1,6 +1,9 @@
 package io.github.ssy.web.controller;
 
 
+import io.github.ssy.drools.DroolsHelper;
+import io.github.ssy.entity.WxUser;
+import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,23 +13,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DangKeController {
 
-    @RequestMapping(value = "/learning/dangke/{id}")
-    public String dangKeDeal(@PathVariable("id") String id, String openId) {
-        //没有走入口数据信息
-        if (StringUtils.isEmpty(openId)) {
-            //请进入公众号，并进行操作。
-            return "/error/error.html";
-        } else {
-            //第一课
-            if (id.equals("1")) {
-                return "redirect:http://h5.cyol.com/special/daxuexi/daxuexi2u7/m.php";
-            }
-        }
-        return "/learning/table.html";
+  @RequestMapping(value = "/learning/dangke/{id}")
+  public String dangKeDeal(@PathVariable("id") String id, String openId) {
+    //没有走入口数据信息
+    if (StringUtils.isEmpty(openId)) {
+      //请进入公众号，并进行操作。
+      return "/error/error.html";
+    } else {
+      //第一课
+      if (id.equals("1")) {
+        return "redirect:http://h5.cyol.com/special/daxuexi/daxuexi2u7/m.php";
+      }
     }
+    return "/learning/table.html";
+  }
 
-    @RequestMapping(value = "/learning/qiandao.html")
-    public String qianDao() {
-        return "/learning/table.html";
-    }
+  @RequestMapping(value = "/learning/qiandao.html")
+  public String qianDao() {
+    return "/learning/table.html";
+  }
+
+  @RequestMapping(value = "/learning/test")
+  @ResponseBody
+  public Object drools() {
+    DroolsHelper droolsHelper = new DroolsHelper();
+    KieSession kieSession = droolsHelper.getKieSession();
+    WxUser wxUser = new WxUser();
+    wxUser.setMsg("nihao");
+    kieSession.insert(wxUser);
+    kieSession.fireAllRules();
+    System.out.println(wxUser);
+    return wxUser;
+  }
+
+
 }
